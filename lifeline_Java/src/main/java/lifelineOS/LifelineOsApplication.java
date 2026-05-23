c2f6c1b09eacf820c452cb0eb0b5e83687bd1003
viswanathgowda <viswavishu40@gmail.com>
2026-05-23 13:55:35 +0530

    Initial commit for new component home page development

---
+++ lifeline_Java/src/main/java/lifelineOS/LifelineOsApplication.java
@@ -0,0 +1,13 @@
+package lifelineOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelineOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelineOsApplication.class, args);
+	}
+
+}
---
+++ lifeline_Java/src/test/java/lifelineOS/LifelineOsApplicationTests.java
@@ -0,0 +1,13 @@
+package lifelineOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelineOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
